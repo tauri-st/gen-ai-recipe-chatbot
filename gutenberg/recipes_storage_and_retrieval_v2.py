4f4765b31788c7520283cc63cde2e43c516f7020
tauri_s <tauristclaire@gmail.com>
2025-09-22 16:39:31 -0700

    Add imports for multi-query retrieval

--- gutenberg/recipes_storage_and_retrieval_v2.py
+++ gutenberg/recipes_storage_and_retrieval_v2.py
@@ -15,12 +15,13 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import SupabaseVectorStore
 from langchain.chains.query_constructor.schema import AttributeInfo
 from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
 from langchain_community.query_constructors.supabase import SupabaseVectorTranslator
 from langchain.chains.query_constructor.base import StructuredQueryOutputParser, get_query_constructor_prompt
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser, BaseOutputParser
 from typing import List
 
 # Supabase
